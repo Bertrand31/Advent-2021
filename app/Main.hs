4ee31b7b4bd20758c73e0a747f3b4062cdaa92b7
Bertrand31 <bertrandjun@gmail.com>
2021-12-21 22:58:51 +0100

    Add Day2_1

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Day1_1
 import Day1_2
+import Day2_1
 
 main :: IO ()
 main = do
@@ -9,3 +10,5 @@ main = do
   putStrLn $ show $ Day1_1.solve $ lines day1_1
   day1_2 <- readFile "day1_2.txt"
   putStrLn $ show $ Day1_2.solve $ lines day1_2
+  day2_1 <- readFile "day2_1.txt"
+  putStrLn $ show $ Day2_1.solve $ lines day2_1
---
+++ src/Day2_1.hs
@@ -0,0 +1,27 @@
+module Day2_1
+    ( solve
+    ) where
+
+import Text.Read (readMaybe)
+
+data Command = Forward Int | Up Int | Down Int
+
+accumulateCommands :: Int -> Int -> [Command] -> Int
+accumulateCommands forward depth [] = forward * depth
+accumulateCommands forward depth (x:xs) =
+  case x of (Forward a) -> accumulateCommands (forward + a) depth xs
+            (Up a)      -> accumulateCommands forward (depth - a) xs
+            (Down a)    -> accumulateCommands forward (depth + a) xs
+
+parseCommand :: String -> Maybe Command
+parseCommand str =
+  case (words str) of "forward":stepsStr:[] -> fmap Forward $ readMaybe stepsStr
+                      "up":stepsStr:[]      -> fmap Up $ readMaybe stepsStr
+                      "down":stepsStr:[]    -> fmap Down $ readMaybe stepsStr
+                      _                     -> Nothing
+
+
+solve :: [String] -> Maybe Int
+solve xs = do
+  commands <- traverse parseCommand xs
+  return $ accumulateCommands 0 0 commands
