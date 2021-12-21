2c30dce95012d365587e2b74b9833454422e95b1
Bertrand31 <bertrandjun@gmail.com>
2021-12-21 21:47:29 +0100

    Refactor day 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,8 @@
 module Main where
 
-import Lib
-import Text.Read (readMaybe)
-import Data.Maybe (catMaybes)
-
-parseDepths :: [String] -> [Int]
-parseDepths = catMaybes . (fmap readMaybe)
-
-countIncreasing :: [(Int, Int)] -> Int
-countIncreasing = length . (filter $ uncurry (<))
-
-makePairs :: [a] -> Maybe [(a, a)]
-makePairs []       = Nothing
-makePairs (x:[])   = Nothing
-makePairs (x:y:[]) = Just [(x, y)]
-makePairs (x:y:xs) = fmap ((x, y):) $ makePairs $ y:xs
+import Day1
 
 main :: IO ()
 main = do
   contents <- readFile "day1.txt"
-  let depths = parseDepths $ lines contents
-  putStrLn $ show $ fmap countIncreasing (makePairs depths)
+  putStrLn $ show $ Day1.solve $ lines contents
---
+++ src/Day1.hs
@@ -0,0 +1,21 @@
+module Day1
+    ( solve
+    ) where
+
+import Text.Read (readMaybe)
+import Data.Maybe (catMaybes)
+
+parseDepths :: [String] -> [Int]
+parseDepths = catMaybes . (fmap readMaybe)
+
+countIncreasing :: [(Int, Int)] -> Int
+countIncreasing = length . (filter $ uncurry (<))
+
+makePairs :: [a] -> Maybe [(a, a)]
+makePairs []       = Nothing
+makePairs (x:[])   = Nothing
+makePairs (x:y:[]) = Just [(x, y)]
+makePairs (x:y:xs) = fmap ((x, y):) $ makePairs $ y:xs
+
+solve :: [String] -> Maybe Int
+solve = (fmap countIncreasing) . makePairs . parseDepths
